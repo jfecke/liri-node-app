9632b73fd12311185d3733a9e8f524abbc892e97
jfecke <jfecke@gmail.com>
2018-12-08 12:44:48 -0600

    got bands api to work

--- liri.js
+++ liri.js
@@ -1,17 +1,34 @@
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var keys = require("./key.js")
 var fs = require("fs");
 var request = require("request");
 var spotify = new Spotify(keys.spotify);
 var input = process.argv[2];
+var subject = process.argv;
 
-console.log(process.argv[3]);
 
 if (input == "concert-this") {
-    var artist = process.argv[3]
+    var stuff = subject.slice(3, subject.length)
+    var artist = ""
+    for (var i in stuff){
+        if (i == stuff.length-1) {
+            artist = artist + stuff[i]
+        } else{
+            artist = artist + stuff[i] + "%20"
+        }
+        
+    }
     var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     request(url, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log(body)
+            var results = JSON.parse(body);
+            for (var i in results) {
+                console.log("---------------")
+                console.log(results[i].venue.name);
+                console.log(results[i].venue.city);
+                console.log(results[i].datetime);
+            }
         } else{
             
         }
@@ -20,13 +37,21 @@ if (input == "concert-this") {
     if (typeof(process.argv[3]) == "undefined") {
         var song = "The Sign"
     } else {
-        var song = process.argv[3];
+        var stuff = subject.slice(3, subject.length)
+        var song = ""
+        for (var i in stuff){
+            if (i == stuff.length-1) {
+                song = song + stuff[i]
+            } else{
+                song = song + stuff[i] + " "
+            }
+    }
     }
     spotify.search({ type: 'track', query: song, market: "US" }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-        console.log(data);
+        console.log(data.tracks.items[0].album.artists);
     })
 } else if (input == "movie-this") {
     var movie = process.argv[3]
@@ -38,7 +63,7 @@ if (input == "concert-this") {
             
         }
     })
-} else if (inpit=="do-what-it-says"){
+} else if (input=="do-what-it-says"){
     fs.readFile("random.txt","utf8", function(err, data){
         if (error) {
             return console.log(error);
